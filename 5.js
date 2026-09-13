80798431e30dbf53bdcc89762e3854bd7b8671f8
manpreetbanger13 <bangermanpreet766@gmail.com>
2026-09-13 01:37:11 -0500

    Add files via upload

---
+++ 5.js
@@ -0,0 +1,13 @@
+let d = document.getElementById("box")
+function change() {
+
+    d.src = "image/mand/IMG_4625.JPG"
+}
+
+function ToNormal() {
+    d.src = "image/mand/IMG_5901.JPG"
+}
+
+function change1(){
+    window.location.href="6.html"
+}
